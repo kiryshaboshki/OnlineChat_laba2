11b967375d2a5bbbc7577ae8361bba8e90963c30
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-01 23:35:45 +1000

    начальный интерфейс(MVVM паттерн) начало сервера

--- ChatClient/MVVM/View/MainWindow.xaml.cs
+++ ChatClient/MVVM/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace onlinechat
+namespace ChatClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
