3a1eb218e728677a8e454cccf335fda5ee99c367
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-06 12:19:48 +1000

    команда подключения к серверу

--- ChatClient/MVVM/ViewModel/MainViewModel.cs
+++ ChatClient/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatClient.MVVM.core;
+using ChatClient.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,13 @@ namespace ChatClient.MVVM.ViewModel
 {
     class MainViewModel
     {
+        public RelayCommand ConnectToServerCommand{ get; set; }
+
+        private Server _server;
         public MainViewModel()
         {
-
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
         }
     }
 }
