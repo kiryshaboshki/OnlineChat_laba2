8955851e92987ecc91021bf6aa9b7462fc384ff6
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-06 13:03:08 +1000

    Добавлены базовые классы для TCP-сервера и клиента

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Collections.Generic;
 
-namespace Programm
+namespace ChatServer
 {
     class Programm
     {
+        static List<Client> _users;
         static TcpListener _listener;
         static void Main(string[] args)
         {
+            _users = new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
             _listener.Start();
 
 
-            var client = _listener .AcceptTcpClient();
-            Console.WriteLine("Client podkl");
+            var client = new Client(_listener.AcceptTcpClient());
+
         }
     }
 }
